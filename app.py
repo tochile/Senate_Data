b30aca67ac3e55a92e907fd9d01dfe1b43849344
Ezekiel Sochima <56741405+tochile@users.noreply.github.com>
2022-12-06 01:47:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,14 +25,14 @@ nltk.download('stopwords')
 
 app = Flask(__name__) 
 Bootstrap(app)
-'''
+
 db = yaml.load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
-'''
+
 
 	
 
